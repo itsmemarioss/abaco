b6f7c202c31e7fa40a1808ef6fabce00a1def3c7
Mário Santos <super.mario.santos@gmail.com>
2016-09-15 19:01:16 -0300

    initial commit

---
+++ src/main/java/br/com/mario/AbacoApplication.java
@@ -0,0 +1,12 @@
+package br.com.mario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacoApplication.class, args);
+	}
+}
