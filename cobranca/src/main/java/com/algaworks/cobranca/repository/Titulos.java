e045384bc62e983541b04899bc0f43a53ad54a89
Mário Santos <super.mario.santos@gmail.com>
2017-02-08 09:25:36 -0300

    correções

--- cobranca/src/main/java/com/algaworks/cobranca/repository/Titulos.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.cobranca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.algaworks.cobranca.model.Titulo;
-
-public interface Titulos extends JpaRepository<Titulo, Long> {
-
-}
