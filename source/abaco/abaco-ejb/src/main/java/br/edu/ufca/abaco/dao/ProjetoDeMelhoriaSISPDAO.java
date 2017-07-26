eac93d88f01dae41d416a645680b8ff54824e819
karloskelvin <karloskelvinsantos@gmail.com>
2017-07-26 14:50:13 -0300

    Criação do CRUD de Projeto de Melhoria (Imcompleto)

---
+++ source/abaco/abaco-ejb/src/main/java/br/edu/ufca/abaco/dao/ProjetoDeMelhoriaSISPDAO.java
@@ -0,0 +1,14 @@
+package br.edu.ufca.abaco.dao;
+
+import br.edu.ufca.abaco.core.dao.DAOGenerico;
+import br.edu.ufca.abaco.core.sisp.ProjetoDeMelhoriaSISP;
+
+/**
+ * Created by karloskelvin on 11/07/17.
+ */
+public class ProjetoDeMelhoriaSISPDAO extends DAOGenerico<ProjetoDeMelhoriaSISP,Long> {
+
+    public ProjetoDeMelhoriaSISPDAO() {
+        super(ProjetoDeMelhoriaSISP.class);
+    }
+}
---
+++ source/abaco/abaco-ejb/src/main/java/br/edu/ufca/abaco/service/ProjetoDeMelhoriaSISPService.java
@@ -0,0 +1,21 @@
+package br.edu.ufca.abaco.service;
+
+import br.edu.ufca.abaco.core.sisp.ProjetoDeMelhoriaSISP;
+import br.edu.ufca.abaco.dao.ProjetoDeMelhoriaSISPDAO;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ * Created by karloskelvin on 11/07/17.
+ */
+@Stateless
+public class ProjetoDeMelhoriaSISPService {
+
+    @Inject
+    ProjetoDeMelhoriaSISPDAO dao;
+
+    public void salvar(ProjetoDeMelhoriaSISP projeto){
+        dao.salvar(projeto);
+    }
+}
