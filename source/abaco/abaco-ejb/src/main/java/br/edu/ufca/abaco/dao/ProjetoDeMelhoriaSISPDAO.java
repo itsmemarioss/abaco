a9f6e53bb6a259ffc0be204a6e5d85491f2c39cf
Mário Santos <super.mario.santos@gmail.com>
2017-08-04 14:16:55 -0300

    ajustando ProjetoDeMelhoriaSISPDAO com EntityManager no construtor

--- source/abaco/abaco-ejb/src/main/java/br/edu/ufca/abaco/dao/ProjetoDeMelhoriaSISPDAO.java
+++ source/abaco/abaco-ejb/src/main/java/br/edu/ufca/abaco/dao/ProjetoDeMelhoriaSISPDAO.java
@@ -1,14 +1,15 @@
 package br.edu.ufca.abaco.dao;
 
-import br.edu.ufca.abaco.core.dao.DAOGenerico;
 import br.edu.ufca.abaco.core.sisp.ProjetoDeMelhoriaSISP;
+
+import javax.persistence.EntityManager;
 
 /**
  * Created by karloskelvin on 11/07/17.
  */
 public class ProjetoDeMelhoriaSISPDAO extends DAOGenerico<ProjetoDeMelhoriaSISP,Long> {
 
-    public ProjetoDeMelhoriaSISPDAO() {
-        super(ProjetoDeMelhoriaSISP.class);
+    public ProjetoDeMelhoriaSISPDAO(EntityManager entityManager) {
+        super(ProjetoDeMelhoriaSISP.class, entityManager);
     }
 }
